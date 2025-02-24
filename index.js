6ea9292b278c19a1c9cc21cc75bedd6731557fb9
Bailee Liana <89488777+baileeliana@users.noreply.github.com>
2025-02-24 12:45:48 -0600

    Add files via upload
    US-03: Game Flow
    1. toggleVisibility() function works as expected/uses hole.classList.toggle() to add/remove the show class
    
    2. showAndHide() calls toggleVisibility() to show/hide a mole after a delay of time using the setTimeout() function provided
    
    3.showUp() function calls setDelay() and chooseHole() to set a delay/hole used to call showAndHide()
    
    4. gameOver() function works as described in the pseudocode provided
    
    5. startGame() function works as expected
    
    US-04: Whack!
    1. Clears the score using `clearScore()`. 
    
     * 2. Sets the game duration using `setDuration()`. 
    
     * 3. Sets up event listeners on the moles using `setEventListeners()`.
    
     * 4. Starts the game timer by calling `startTimer()`.  
    
     * 5. Begins the game loop by calling `showUp()` to display moles. 
    
    US-05: Timer:
    1. startTimer() and updateTimer() work as expected
    
    2. timer displays in the game.

---
+++ index.js
@@ -0,0 +1,230 @@
+//-----------------BASIC GAME STRUCTURE------------------//
+const holes = document.querySelectorAll('.hole');
+const moles = document.querySelectorAll('.mole');
+const startButton = document.querySelector('#start');
+const score = document.querySelector('#score');
+const timerDisplay = document.querySelector('#timerDisplay');
+
+let time = 0;
+let timer;
+let lastHole = 0;
+let points = 0;
+let difficulty = "hard";
+
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+//------------BASIC GAME FUNCTIONALITY: RANDOMNESS--------//
+
+//1.create random integers for length of mole airtime
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//2. setDelay(difficulty)
+function setDelay(difficulty) {
+  if(difficulty === hard) {
+    randomInteger(600, 1200);
+  if (difficulty === normal) {
+    return 1000;
+  } if (difficulty === easy) {
+    return 1500;
+  } 
+}}
+
+//3. selecting a random hole
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function chooseHole(holes) {
+  const index = randomInteger(0, 2);
+  const hole = holes[index];
+  if (hole === lastHole) {
+    return chooseHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+/*TEST FUNCTION CHOOSEHOLE
+let hole1 = chooseHole(holes);
+
+hole1.classList.toggle("highlight");
+console.log(hole1.innerHTML);
+console.log(hole1.classList);
+
+hole1 = chooseHole(holes);
+hole1.classList.toggle("highlight");
+console.log(hole1.innerHTML);
+console.log(hole1.classList);
+*/
+
+//Calls the showUp function if time > 0 and stops the game if time = 0.
+/*
+* The purpose of this function is simply to determine if the game should
+* continue or stop. The game continues if there is still time `if(time > 0)`.
+* If there is still time then `showUp()` needs to be called again so that
+* it sets a different delay and a different hole. If there is no more time
+* then it should call the `stopGame()` function. The function also needs to
+* return the timeoutId if the game continues or the string "game stopped"
+* if the game is over.
+*/
+  
+/*  if time > 0:
+  *  //   timeoutId = showUp()
+  *  //   return timeoutId
+  *  // else
+  *  //   gameStopped = stopGame()
+  *  //   return gameStopped
+*/
+
+
+//------------------------GAME FLOW---------------------//
+//4. determine if game should continue/stop
+function gameOver() {
+  if (time > 0) {
+    timeoutId = showUp();
+    return timeoutId
+  }
+  else {
+    gameStopped = stopGame();
+  }
+  return gameStopped;
+}
+
+//3. call showAndHide function
+function showUp() {
+  let delay = setDelay();
+  const hole = chooseHole();
+  return showAndHide(hole, delay);
+}
+
+//2. show/hide mole given delay time/hole where mole is at
+function showAndHide(hole, delay) {
+  toggleVisibility(hole);
+
+  const timeoutID = setTimeout(() => {
+    toggleVisibility(hole);
+    gameOver();
+  }, delay);
+  return timeoutID;
+}
+
+//1. adds/removes 'show' class from style.css & return hole
+function toggleVisibility(hole) {
+  hole.classList.toggle('show'); 
+  return hole;
+}
+
+showUp();
+
+//--------------------------WHACK!-----------------------//
+//--------------------------TIMER------------------------//
+
+//WHACK: 1. increment global points variable/update scoreboard
+function updateScore() {
+  points ++;
+  score.textContent = points;
+  return points;
+}
+
+//WHACK: 2. resets scoreboard to 0 so player can play again
+function clearScore() {
+  points = 0;
+  score.textContent = points;
+  return points;
+}
+
+//TIMER: 2. updates the control board with the timer if time > 0
+function updateTimer() {
+  if (time > 0) {
+    time -= 1;
+    timerDisplay.textContent = time;
+  }
+  return time;
+}
+ 
+//TIMER: 1. starts timer
+function startTimer() {
+  timer = setInterval(updateTimer, 1000);
+  return timer;
+}
+
+
+//WHACK: 3. event handler that calls updateScore() to increment score if mole was clicked by player
+function whack(event) {
+  mole.addEventListener('click', whack);
+  updateScore();
+  return points;
+}
+
+//WHACK: 4. event handler gets called when player clicks a mole
+function setEventListeners() {
+  moles.forEach(mole =>
+    mole.addEventListener('click', whack));
+  return moles;
+}
+
+setEventListeners();
+
+//TIMER: 3. sets time limit in seconds of game
+function setDuration(duration) {
+  time = duration;
+  return time;
+}
+
+//TIMER: 4. when game is stopped the timer gets cleared
+function stopGame(){
+  stopAudio(song); 
+  clearInterval(timer);
+  return "game stopped";
+}
+
+//----------------------START GAME-----------------------//
+function startGame(){
+  clearScore();
+  stopGame();  
+  setDuration(10);
+  setEventListeners();
+  startTimer();
+  showUp();
+  return "game started";
+}
+
+startButton.addEventListener("click", startGame);
+
+
+//Please do not modify the code below.
+//Used for testing purposes.
+window.randomInteger = randomInteger;
+window.chooseHole = chooseHole;
+window.setDelay = setDelay;
+window.startGame = startGame;
+window.gameOver = gameOver;
+window.showUp = showUp;
+window.holes = holes;
+window.moles = moles;
+window.showAndHide = showAndHide;
+window.points = points;
+window.updateScore = updateScore;
+window.clearScore = clearScore;
+window.whack = whack;
+window.time = time;
+window.setDuration = setDuration;
+window.toggleVisibility = toggleVisibility;
+window.setEventListeners = setEventListeners;
